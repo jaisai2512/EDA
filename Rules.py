50c2cdc873776ee022d759fef527137cf4f52464
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-12 02:11:27 +0530

    Update Rules.py

--- Rules.py
+++ Rules.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def validate_obj(column):
     if column.isna().all():
         return 'All rows are null'
@@ -13,16 +12,26 @@ def validate_obj(column):
 def extract_data(data: pd.DataFrame) -> dict:
     num_rows, num_columns = data.shape
 
-    # Extracting data types of each column
     column_data_types = {
         column: validate_obj(data[column]) if data[column].dtype == 'O' else data[column].dtype
         for column in data.columns
+    }
+
+    mean = {
+        column:round(data[column].mean(),2)  if data[column].dtype in ['int64','float64',int,float] else data[column].dtype
+        for column in data.columns
+    }
+
+    no_null = {
+        column: data[column].isnull().sum() for column in data.columns
     }
 
     summary = {
         'num_rows': num_rows,
         'num_columns': num_columns,
         'column_names_data_types': column_data_types,
+        'mean' : mean,
+        'num_of_null' : no_null
     }
 
     return summary
