60f0e2131e717759734ec707559169afc6c67017
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-12 02:33:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,18 +18,15 @@ if uploaded_file is not None:
     st.write("Summary of the CSV file:")
     summary = Rules.extract_data(df)
 
+
     prompt = f'''You are an English teacher with expertise in data interpretation with pandas dataframe.You are provided with a dataframe represented as a dictionary. Your task is to analyze the data thoroughly and provide a comprehensive summary in paragraph form. The summary should be rich, compact, and concise, capturing all key information and insights from the data. Ensure that no critical details are omitted while keeping the text engaging.
     
     Instruction:
     1.No code should be generated
     2.No tabular columns should be generrated
     3.No information apart from the information obtained from the dictionary.
-
-    Example summary:
-    The fruit dataset contains information about various fruits, featuring 5 fields: Fruit_Name, Color, Weight_in_grams, Sweetness_Level, and Country_of_Origin. Fruit_Name identifies the type of fruit, while Color indicates its appearance. Weight_in_grams provides the weight, Sweetness_Level rates the taste on a scale, and Country_of_Origin shows where the fruit is typically grown. This dataset can be used for classification and analysis tasks related to fruit characteristics.
     Dictionary: {summary}
     '''
-
 
     # Display basic information about the CSV
     st.write("Basic Information:")
