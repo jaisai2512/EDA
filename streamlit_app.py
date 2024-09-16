1348fc222d3e7225632d463f90502798e5307f6b
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-17 01:14:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,14 @@ if uploaded_file is not None:
     summary = Rules.extract_data(df)
 
 
-    prompt = f'''You are an English teacher with expertise in data interpretation with pandas dataframe.You are provided with a dataframe represented as a dictionary. Your task is to analyze the data thoroughly and provide a comprehensive summary in paragraph form. The summary should be rich, compact, and concise, capturing all key information and insights from the data. Ensure that no critical details are omitted while keeping the text engaging.
+    prompt = f'''You are a data analyst with expertise in data interpretation using pandas dataframe. You are provided with a dataframe represented as a dictionary. Thoroughly analyze this data and provide a comprehensive summary in a single paragraph. The summary should be rich, compact, and concise, capturing all key information and insights from the data. Ensure that no critical details are omitted while keeping the text engaging.
+
+  Please adhere to the following instructions:
+    1.Do not generate any code.
+    2.Do not generate tabular columns.
+    3.Only use information obtained from the dictionary provided.
     
-    Instruction:
-    1.No code should be generated
-    2.No tabular columns should be generrated
-    3.No information apart from the information obtained from the dictionary.
-    Dictionary: {summary}
+  Here is the dictionary for analysis:{summary}
     '''
 
     # Display basic information about the CSV
