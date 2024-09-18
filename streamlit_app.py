d47db7ac1cfdb7a789f515886c47b9b5b3ffd3a6
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-19 00:30:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ Here is the summary of the data:
 
     # Display basic information about the CSV
     st.write("Basic Information:")
-    st.write(api(promp_qa))
+    st.write(api(prompt_qa))
 else:
     st.write("Please upload a CSV file to proceed.")
 
