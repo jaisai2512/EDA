87ab8d344a2a61c48d422ee33a1d51d56a8b3b67
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-21 11:36:23 +0530

    Update execute.py

--- execute.py
+++ execute.py
@@ -3,11 +3,11 @@ import os
 def execute_code_safely(code,var_dict):
         try:
             # Execute the code in a subprocess
-            result = exec(code.replace('```python','').replace('```',''),var_dict)
-            
             
             # Check if 'output.png' was created
             with tempfile.TemporaryDirectory() as tmp_dir:
+              var_dict['tmp_dir'] = tmp_dir
+              result = exec(code.replace('```python','').replace('```',''),var_dict)      
               output_image_path = os.path.join(tmp_dir, 'output.png')
               if os.path.exists(output_image_path):
                 with open(output_image_path, 'rb') as img_file:
