75e569d378b0988b25d516a3ee2186135d68d37b
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-22 20:03:06 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,59 @@
+def get_template(library):
+
+        general_instructions = f"If the solution requires a single value (e.g. max, min, median, first, last etc), ALWAYS add a line (axvline or axhline) to the chart, ALWAYS with a legend containing the single value (formatted with 0.2F). If using a <field> where semantic_type=date, YOU MUST APPLY the following transform before using that column i) convert date fields to date types using data[''] = pd.to_datetime(data[<field>], errors='coerce'), ALWAYS use  errors='coerce' ii) drop the rows with NaT values data = data[pd.notna(data[<field>])] iii) convert field to right time format for plotting.  ALWAYS make sure the x-axis labels are legible (e.g., rotate when needed). Solve the task  carefully by completing ONLY the <imports> AND <stub> section. Given the dataset summary, the plot(data) method should generate a {library} chart ({goal.visualization}) that addresses this goal: {goal.question}. DO NOT WRITE ANY CODE TO LOAD THE DATA. The data is already loaded and available in the variable data."
+
+        matplotlib_instructions = f" {general_instructions} DO NOT include plt.show(). The plot method must return a matplotlib object (plt). Think step by step. \n"
+
+        if library == "matplotlib":
+            instructions = {
+                "role": "assistant",
+                "content": f"  {matplotlib_instructions}. Use BaseMap for charts that require a map. "}
+            template = \
+                f"""
+import matplotlib.pyplot as plt
+import pandas as pd
+<imports>
+# plan -
+def plot(df: pd.DataFrame):
+    <stub> # only modify this section
+    plt.title('{goal.question}', wrap=True)
+    return plt;
+
+chart = plot(df) # data already contains the data to be plotted. Always include this line. No additional code beyond this line."""
+        elif library == "seaborn":
+            instructions = {
+                "role": "assistant",
+                "content": f"{matplotlib_instructions}. Use BaseMap for charts that require a map. "}
+
+            template = \
+                f"""
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+<imports>
+# solution plan
+# i.  ..
+def plot(df: pd.DataFrame):
+
+    <stub> # only modify this section
+    plt.title('{goal.question}', wrap=True)
+    return plt;
+
+chart = plot(df) # data already contains the data to be plotted. Always include this line. No additional code beyond this line."""
+
+
+        elif library == "altair":
+            instructions = {
+                "role": "system",
+                "content": f"{general_instructions}. Always add a type that is BASED on semantic_type to each field such as :Q, :O, :N, :T, :G. Use :T if semantic_type is year or date. The plot method must return an altair object (chart)`. Think step by step. \n",
+            }
+            template = \
+                """
+import altair as alt
+<imports>
+def plot(df: pd.DataFrame):
+    <stub> # only modify this section
+    return chart
+chart = plot(df) # data already contains the data to be plotted.  Always include this line. No additional code beyond this line..
+"""
+          return template, instructions
