6d49672c18c0f2f1dedb57d4008e1773f43e63c8
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-22 20:21:36 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+
 def get_template(library):
 
         general_instructions = f"If the solution requires a single value (e.g. max, min, median, first, last etc), ALWAYS add a line (axvline or axhline) to the chart, ALWAYS with a legend containing the single value (formatted with 0.2F). If using a <field> where semantic_type=date, YOU MUST APPLY the following transform before using that column i) convert date fields to date types using data[''] = pd.to_datetime(data[<field>], errors='coerce'), ALWAYS use  errors='coerce' ii) drop the rows with NaT values data = data[pd.notna(data[<field>])] iii) convert field to right time format for plotting.  ALWAYS make sure the x-axis labels are legible (e.g., rotate when needed). Solve the task  carefully by completing ONLY the <imports> AND <stub> section. Given the dataset summary, the plot(data) method should generate a {library} chart ({goal.visualization}) that addresses this goal: {goal.question}. DO NOT WRITE ANY CODE TO LOAD THE DATA. The data is already loaded and available in the variable data."
@@ -35,7 +36,10 @@ import matplotlib.pyplot as plt
 def plot(df: pd.DataFrame):
 
     <stub> # only modify this section
-    return plt;
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)  # Move the cursor to the start of the stream
+    return buf
 
 chart = plot(df) # data already contains the data to be plotted. Always include this line. No additional code beyond this line."""
 
@@ -51,7 +55,10 @@ import altair as alt
 <imports>
 def plot(df: pd.DataFrame):
     <stub> # only modify this section
-    return chart
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)  # Move the cursor to the start of the stream
+    return buf
 chart = plot(df) # data already contains the data to be plotted.  Always include this line. No additional code beyond this line..
 """
           return template, instructions
