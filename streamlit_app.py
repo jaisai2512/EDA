17b6fb8934441389e5e30c722f580d14997d4ea6
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-22 13:09:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from execute import execute_code_safely
 import seaborn as sns
 import matplotlib.pyplot as plt 
 import io
-
+from Summary import summary_gen
  
 # Title of the Streamlit app
 st.title("EDA Report Generator")
@@ -22,19 +22,10 @@ if uploaded_file is not None:
 
     # Display the content of the CSV file
     st.write("Summary of the CSV file:")
-    summary = Rules.extract_data(df)
 
     var_dict = {'df':df}
 
-    prompt = f'''You are a data analyst with expertise in data interpretation using pandas dataframe. You are provided with a dataframe represented as a dictionary. Thoroughly analyze this data and provide a comprehensive summary in a single paragraph. The summary should be rich, compact, and concise, capturing all key information and insights from the data. Ensure that no critical details are omitted while keeping the text engaging.
-
-  Please adhere to the following instructions:
-    1.Do not generate any code.
-    2.Do not generate tabular columns.
-    3.Only use information obtained from the dictionary provided.
-    
-  Here is the dictionary for analysis:{summary}
-    '''
+    summary = summary_gen(df)
     prompt_qa = f'''You are a data analyst with expertise in interpreting data summaries and generating insightful questions to identify patterns in data. Use visualizations to support your analysis and provide meaningful interpretations of the trends. Please adhere to the following instructions:
 
 1. Do not generate any code.
